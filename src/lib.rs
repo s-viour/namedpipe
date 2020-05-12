e5dd4994fcd6707f939d5108fdf1911201b8ab5f
matt wyatt <mwyatt1000@gmail.com>
2020-05-12 12:08:52 -0400

    fixed EEXIST errors from preventing a pipe from being created

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ use std::fs::{OpenOptions, File};
 use std::path::Path;
 use std::io::{self, Write, Read, Seek, SeekFrom};
 
-pub use nix::unistd;
-pub use nix::sys::stat::Mode;
+use nix::unistd;
+use nix::sys::stat::Mode;
+use nix::errno::Errno;
 
 
 pub struct NamedPipe {
@@ -16,7 +17,27 @@ pub struct NamedPipe {
 
 impl NamedPipe {
 	pub fn new<P: AsRef<Path>>(path: P, mode: Mode) -> Result<NamedPipe> {
-		unistd::mkfifo(path.as_ref(), mode)?;
+		/* grab the result of mkfifo */
+		let mkfifo_result = unistd::mkfifo(path.as_ref(), mode);
+
+		/* we need to ignore any EEXIST errors that occur,
+		 * because they're okay in this context
+		 * so first, we check if the result is an error */
+		if mkfifo_result.is_err() {
+			/* if it is, then we destructure the error enum */
+			match mkfifo_result.err().unwrap() {
+				/* in the case that it's of variant Sys(errno) */
+				nix::Error::Sys(e) => {
+					/* check and see if the errno is EEXIST */
+					if e != Errno::EEXIST {
+						/* if it ISN'T EEXIST, then actually produce an error */
+						mkfifo_result?
+					}
+				}
+				/* in any other variant, it's an actual error, so produce one */
+				_ => mkfifo_result?
+			}
+		}
 
 		let file = OpenOptions::new()
 			.create(true)
@@ -27,6 +48,10 @@ impl NamedPipe {
 		Ok(NamedPipe {
 			file,
 		})
+	}
+
+	pub fn file(&mut self) -> &mut File {
+		&mut self.file
 	}
 }
 
